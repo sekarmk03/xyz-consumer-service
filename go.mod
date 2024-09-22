0c2710d66503841832c6203ce87f7aea33818a8e
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-09-22 14:41:55 +0700

    feat: add consumer repository

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.23
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
---
+++ modules/consumer/internal/repository/consumer_repository.go
@@ -0,0 +1,116 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+	"xyz-consumer-service/modules/consumer/entity"
+
+	"go.opencensus.io/trace"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
+)
+
+type ConsumerRepository struct {
+	db *gorm.DB
+}
+
+func NewConsumerRepository(db *gorm.DB) *ConsumerRepository {
+	return &ConsumerRepository{
+		db: db,
+	}
+}
+
+type ConsumerRepositoryUseCase interface {
+	FindAll(ctx context.Context, req any) ([]*entity.Consumer, error)
+	FindById(ctx context.Context, id uint64) (*entity.Consumer, error)
+	Create(ctx context.Context, req *entity.Consumer) (*entity.Consumer, error)
+	Update(ctx context.Context, consumer *entity.Consumer, updatedFields map[string]interface{}) (*entity.Consumer, error)
+	Delete(ctx context.Context, id uint64) error
+}
+
+func (c *ConsumerRepository) FindAll(ctx context.Context, req any) ([]*entity.Consumer, error) {
+	ctxSpan, span := trace.StartSpan(ctx, "ConsumerRepository - FindAll")
+	defer span.End()
+
+	var consumers []*entity.Consumer
+	if err := c.db.Debug().WithContext(ctxSpan).Order("created_at desc").Find(&consumers).Error; err != nil {
+		log.Println("ERROR: [ConsumerRepository - FindAll] Internal server error:", err)
+		return nil, err
+	}
+
+	return consumers, nil
+}
+
+func (c *ConsumerRepository) FindById(ctx context.Context, id uint64) (*entity.Consumer, error) {
+	ctxSpan, span := trace.StartSpan(ctx, "ConsumerRepository - FindById")
+	defer span.End()
+
+	var consumer entity.Consumer
+	if err := c.db.Debug().WithContext(ctxSpan).Where("id = ?", id).First(&consumer).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("WARNING: [ConsumerRepository - FindById] Consumer not found for id:", id)
+			return nil, status.Errorf(codes.NotFound, "Consumer not found for id: %v", id)
+		}
+		log.Println("ERROR: [ConsumerRepository - FindById] Internal server error:", err)
+		return nil, err
+	}
+
+	return &consumer, nil
+}
+
+func (c *ConsumerRepository) Create(ctx context.Context, req *entity.Consumer) (*entity.Consumer, error) {
+	ctxSpan, span := trace.StartSpan(ctx, "ConsumerRepository - Create")
+	defer span.End()
+
+	if err := c.db.Debug().WithContext(ctxSpan).Create(req).Error; err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			log.Println("WARNING: [ConsumerRepository - Create] Consumer already exists for nik:", req.Nik)
+			return nil, status.Errorf(codes.AlreadyExists, "Consumer already exists for nik: %v", req.Nik)
+		}
+		log.Println("ERROR: [ConsumerRepository - Create] Internal server error:", err)
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (c *ConsumerRepository) Update(ctx context.Context, consumer *entity.Consumer, updatedFields map[string]interface{}) (*entity.Consumer, error) {
+	ctxSpan, span := trace.StartSpan(ctx, "ConsumerRepository - Update")
+	defer span.End()
+
+	updatedFields["updated_at"] = time.Now()
+
+	if err := c.db.Debug().WithContext(ctxSpan).Model(&consumer).Updates(updatedFields).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("WARNING: [ConsumerRepository - Update] Consumer not found for id:", consumer.Id)
+			return nil, status.Errorf(codes.NotFound, "Consumer not found for id: %v", consumer.Id)
+		}
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			log.Println("WARNING: [ConsumerRepository - Update] Consumer already exists for nik:", consumer.Nik)
+			return nil, status.Errorf(codes.AlreadyExists, "Consumer already exists for nik: %v", consumer.Nik)
+		}
+		log.Println("ERROR: [ConsumerRepository - Update] Internal server error:", err)
+		return nil, err
+	}
+
+	return consumer, nil
+}
+
+func (c *ConsumerRepository) Delete(ctx context.Context, id uint64) error {
+	ctxSpan, span := trace.StartSpan(ctx, "ConsumerRepository - Delete")
+	defer span.End()
+
+	if err := c.db.Debug().WithContext(ctxSpan).Where("id = ?", id).Delete(&entity.Consumer{}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("WARNING: [ConsumerRepository - Delete] Consumer not found for id:", id)
+			return status.Errorf(codes.NotFound, "Consumer not found for id: %v", id)
+		}
+		log.Println("ERROR: [ConsumerRepository - Delete] Internal server error:", err)
+		return err
+	}
+
+	return nil
+}
